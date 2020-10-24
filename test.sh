64eaeb3d4d55c770ea973aca718827872eccdad1
alberto56 <albert@mediatribe.net>
2020-10-24 09:06:58 -0400

    Add helper output to test file

--- test.sh
+++ test.sh
@@ -2,5 +2,8 @@ set -e
 docker pull node
 docker build -t local-dcycle-yaml-lint-image .
 
+echo " => Smoke test (--help)"
 docker run -v $(pwd)/examples:/app/code local-dcycle-yaml-lint-image --help
+echo " => Linting some dummy yaml"
 docker run -v $(pwd)/examples:/app/code local-dcycle-yaml-lint-image code/yaml.yaml
+echo " => Done"
